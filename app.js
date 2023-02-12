9f82ec5e3f08b529f2896f9d316997c10cd4c0bd
Shun <ter0123540@gmail.com>
2023-02-12 23:42:52 +0800

    feat: connect to DB

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 
 const app = express();
+
+// connect to DB
+const DB = process.env.DATABASE.replace(
+  "<password>",
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB).then(() => {
+  console.log("connect to DB successfully");
+});
 
 app.use(logger("dev"));
 app.use(express.json());
