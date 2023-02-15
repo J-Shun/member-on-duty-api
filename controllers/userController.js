758f21a521ee885c33541d3849280c25846591a9
Shun <ter0123540@gmail.com>
2023-02-15 13:26:46 +0800

    feat: add login api

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,28 +14,28 @@ const userSignup = handleErrorAsync(async (req, res, next) => {
   }
 
   if (password !== confirmPassword) {
-    return appError("400", "password check fails", next);
+    return appError("400", "Password check fails", next);
   }
 
   if (!validator.isLength(password, { min: 8 })) {
-    return appError("400", "password must contain at least 8 letters", next);
+    return appError("400", "Password must contain at least 8 letters", next);
   }
 
   if (!validator.isEmail(email)) {
-    return appError("400", "invalid email address", next);
+    return appError("400", "Invalid email address", next);
   }
 
   if (name.trim().length === 0) {
-    return appError("400", "name must not be empty", next);
+    return appError("400", "Name must not be empty", next);
   }
 
   if (name.trim().length > 20) {
-    return appError("400", "please fill in less than 20 characters", next);
+    return appError("400", "Please fill in less than 20 characters", next);
   }
 
   const emailExit = await User.exists({ email });
   if (emailExit) {
-    return appError("400", "email already in use", next);
+    return appError("400", "Email already in use", next);
   }
 
   // encrypt
@@ -56,6 +56,45 @@ const userSignup = handleErrorAsync(async (req, res, next) => {
   });
 });
 
+const userLogin = handleErrorAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // validation
+  if (!email || !password) {
+    return appError("400", "All fields must be filled", next);
+  }
+
+  if (!validator.isLength(password, { min: 8 })) {
+    return appError("400", "Invalid password format", next);
+  }
+
+  if (!validator.isEmail(email)) {
+    return appError("400", "Invalid email format", next);
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return appError("400", "Email not found");
+  }
+
+  console.log(user);
+
+  // console.log(user.password);
+
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return appError("400", "Incorrect password");
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_DAY,
+  });
+
+  res.status(200).json({ status: "success", email, token });
+});
+
 module.exports = {
   userSignup,
+  userLogin,
 };
--- routes/user.js
+++ routes/user.js
@@ -1,13 +1,9 @@
 const express = require("express");
-const { userSignup } = require("../controllers/userController");
+const { userSignup, userLogin } = require("../controllers/userController");
 const router = express.Router();
 
 // signup
-router.post("/signup", function (req, res, next) {
-  res.json({
-    mssg: "signup",
-  });
-});
+router.post("/signup", userLogin);
 
 // login
 router.post("/login", userSignup);
